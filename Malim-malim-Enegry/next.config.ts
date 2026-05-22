515d61e7de316e534f5b18a944a710f4c150dfa4
sherryjjj <xuexuets@foxmail.com>
2026-05-22 14:36:32 +0200

    chore: remove Malim-malim-Enegry folder and rename project to swiss-pro

--- Malim-malim-Enegry/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.178.112'],
-  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
-};
-
-export default nextConfig;
